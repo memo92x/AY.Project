bdb6b6697ea4f7f3bd9fa7f51eeaff646de8c328
Haci Mehmet Yilmaz <yilmaz1992@hotmail.com>
2016-07-01 17:23:39 +0200

    RestuarantManager og partial

---
+++ AY.Web/Models/Managers/RestaurantManager.cs
@@ -0,0 +1,31 @@
+﻿using conString;
+using System.Collections.Generic;
+
+namespace AY.Web.Models.Managers
+{
+    public class RestaurantManager
+    {
+        private static RestaurantManager instance;
+        private conStringDB _db;
+
+        public RestaurantManager()
+        {
+            _db = new conStringDB();
+        }
+
+        public static RestaurantManager Instance
+        {
+            get
+            {
+                if (instance == null) instance = new RestaurantManager();
+
+                return instance;
+            }
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantById(int restaurantId)
+        {
+            return _db.Fetch<Restaurant>("SELECT * FROM RESTAURANTS WHERE ID=@0", restaurantId);
+        }
+    }
+}
---
+++ AY.Web/Models/Partials/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using conString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AY.Web.Models.Partials
+{
+    public class Restaurants : Restaurant
+    {
+    }
+}
